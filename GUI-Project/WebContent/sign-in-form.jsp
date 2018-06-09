62f4d0ccd9a5f09ab8eb9095a196f7f401395824
Erwin Herrera <Erwin.Herrera97@gmail.com>
2018-06-09 13:57:23 -0500

    Initial commit

---
+++ GUI-Project/WebContent/sign-in-form.jsp
@@ -0,0 +1,24 @@
+<html>
+
+<head><title>Sign In</title></head>
+
+<body>
+
+<form action="SignInServlet" method="GET">
+
+	Username: <input type="text" name="userName" /> <!-- Form for first name -->
+	
+	<br/><br/> <!-- line break -->
+	
+	Password: <input type="password" name="password" /> <!-- Form for last name  -->
+	
+	<br/><br/> <!-- line break -->
+	
+	<input type="submit" value="Submit" />	<!-- Submit button  -->
+	
+
+</form>
+
+</body>
+
+</html>
---
+++ GUI-Project/src/com/controller/SignInServlet.java
@@ -0,0 +1,64 @@
+package com.controller;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class StudentServlet
+ */
+@WebServlet("/SignInServlet")
+public class SignInServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public SignInServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+    
+    
+    
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		// Step 1: set content type
+		response.setContentType("text/html");
+		
+		// Step 2: get the printwriter
+		PrintWriter out = response.getWriter();
+		
+		// Step 3: generate the HTML content
+		out.println("<html><body>");
+		
+		out.println("The customer is confirmed: "
+					+ request.getParameter("userName"));		
+		out.println("</br></br>");
+		out.println("<a href=\"Website.html\">Return to homepage.</a>");
+		out.println("</body></html>");
+		
+	}
+	
+	
+	
+	
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
