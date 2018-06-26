900df6c267dfda442f8e1e05a1e628d7d79ba480
JeromeD123 <jerome.daly26@gmail.com>
2018-06-26 17:37:22 -0500

    added basic constructor

--- GUI-Project/src/application/model/Customer.java
+++ GUI-Project/src/application/model/Customer.java
@@ -1,15 +1,30 @@
 package application.model;
 
 /**
- * 
+ *
  * @author Erwin Herrera
- * 
+ *
  * This class will be responsible for
  * keeping the information of the customer.
- * 
+ *
  * (i.e. Name, password info, etc)
  */
 
 public class Customer {
+  //customer name
+  String name, address;
+  int phoneNum;
 
+  Cart cart = new Cart;
+
+  //constructor
+  public void customer(String name, address, int phoneNum){
+    this.name = name;
+    this.address = address;
+    this.phoneNum = phoneNum;
+  }
+
+  //todo: Add customer getters and setters
+
+  //Do we want to ask for customer data? Name, address, email?
 }
