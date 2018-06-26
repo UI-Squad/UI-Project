03c06205ab64ba42cd02ed6360049c8931225cb6
JeromeD123 <jerome.daly26@gmail.com>
2018-06-26 18:35:52 -0500

    made addToCart function for shopping

--- GUI-Project/src/application/model/Cart.java
+++ GUI-Project/src/application/model/Cart.java
@@ -1,5 +1,14 @@
 package application.model;
 
-
 //This file will be the model for the
 //applications shopping Cart
+public class Cart{
+
+  //create an array for the cart
+
+  //add an item to the cart array
+  public void addToCart(Item item){
+    this.cartArray.add(item);
+  }
+  
+}
